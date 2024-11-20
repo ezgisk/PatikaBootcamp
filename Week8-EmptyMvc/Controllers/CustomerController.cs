8cf4e8e411b09127812e6686ea32aa0b9bf24572
Ezgi Işık Geçimli <ezgisk97@gmail.com>
2024-11-19 22:54:42 -0700

    First mvc project added.

---
+++ Week8-EmptyMvc/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week8_EmptyMvc.Models;
+
+namespace Week8_EmptyMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+
+            // ViewModel oluşturup view'a gönderiyoruz
+            var viewModel = new CustomerViewModel
+            {
+                Customer = customer,
+                WelcomeMessage = "Welcome to our customer portal!"
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Week8-EmptyMvc/Models/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Week8_EmptyMvc.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public string WelcomeMessage {  get; set; }
+    }
+}
