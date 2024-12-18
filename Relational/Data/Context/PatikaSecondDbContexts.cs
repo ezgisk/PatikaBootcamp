e3daadce99b6154267ea7eb2dda4623b3533bf14
Ezgi Işık Geçimli <ezgisk97@gmail.com>
2024-12-17 23:52:31 -0700

    PatikaSecondDbContexts added

---
+++ Relational/Data/Context/PatikaSecondDbContexts.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relational.Data.Entity;
+
+namespace Relational.Data.Context
+{
+    public class PatikaSecondDbContexts : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // PostgreSQL bağlantı dizesi
+            optionsBuilder.UseNpgsql("Host=localhost;Database=PatikaCodeFirstDb2;Username=postgres;Password=7777");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // User-Post ilişkisi
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts) 
+                .WithOne(p => p.User)  
+                .HasForeignKey(p => p.UserId); 
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Relational/Data/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Relational.Data.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        // İlişki
+        public ICollection<Post> Posts { get; set; }
+    }
+}
